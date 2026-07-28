0b983f0acee082522b3fa95ba4ebe507660de53f
Abdullah Koyuncu <wisewebworks@outlook.com>
2026-07-28 16:41:34 +0200

    security: fix Vulnerabilities
    - CVE-2026-12590
    - CVE-2026-59869
    - CVE-2025-68470

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,12 +1,9 @@
-import js from "@eslint/js";
 import globals from "globals";
 
 export default [
   {
     ignores: ["dist", "node_modules"],
   },
-
-  js.configs.recommended,
 
   {
     files: ["**/*.js"],
